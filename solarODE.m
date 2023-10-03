b69f49c392086e0d6b89e74a8cc922a57b1a7fb7
Dokyun Kim <donny5557@gmail.com>
2023-10-03 11:18:23 -0400

    Skeleton for Solar ODE

---
+++ solarODE.m
@@ -0,0 +1,50 @@
+function dTdt = solarODE(t,T)
+    
+    %Thickness of insulator
+    L_ins = 
+    %Thickness of wall
+    L_wall =
+    %Thickness of absorber
+    L_abs = 
+
+    %Heat transfer coeff of window
+    h_win = 1.4;
+    %Heat transfer coeff of inside
+    h_in = 15;
+    %Heat transfer coeff of outside
+    h_out = 30;
+
+    %Area of window
+    A_win = 
+    %Area of wall
+    A_wall = 
+    %Area of insulator
+    A_ins = 
+    %Area of absorber
+    A_abs = 
+
+
+    %R absorber to air in
+    R0= 
+    %R air in to insulator
+    R1=
+    %R air in to window
+    R2= 1/(h_win*A_win);
+    %R through window
+    R3 = 
+    %R through insulator
+    R4=
+    %R through outer wall
+    R5=
+    %R wall to air out
+    R6 = 
+    %R window to air out
+    R7=
+    %C absorber
+    C= 
+    
+    
+
+    dTdt = []
+
+end

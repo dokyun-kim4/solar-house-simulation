45f1ff945cf99c1773c9129cd7eec52be8df1eb7
Dokyun Kim <donny5557@gmail.com>
2023-10-13 10:32:10 -0400

    more constants

--- solarODE.m
+++ solarODE.m
@@ -1,22 +1,26 @@
-function dTdt = solarODE(t,T)
+function dTdt = solarODE(t,T)  
+    %Temperature
+    T_out = -3;
+    
+    % ALL MEASURED FROM INSIDE
     %height of house
     height = 4.57;
     %width of house
-    width = 5.49;
+    width = 5.94;
     %depth of house
-    depth = 5.94;
+    depth = 5.49;
 
     %Thickness of insulator
-    L_ins = 
+    L_ins = 0.2; 
     %Thickness of wall
-    L_wall =
+    L_wall = 0.2;
     %Thickness of absorber
-    L_abs = 
+    L_abs = 0.4;
 
     %thermal conductivity of insulator
-    k_ins =
+    k_ins = 0.04;
     %thermal cond of outer wall
-    k_wall=
+    k_wall= 35;
 
     %Heat transfer coeff of window
     h_win = 1.4;
@@ -27,8 +31,12 @@ function dTdt = solarODE(t,T)
 
     %Area of window
     A_win = (sin(deg2rad(72)*height)*depth);
+    %Area of one side wall
+    A_side = (2*(L_wall + L_ins) + width) * (2*(L_wall + L_ins) + L_abs + height);
+    % Area of one top/bottom
+    A_top = (2*(L_wall + L_ins) + width) * (2*(L_wall + L_ins) + depth); 
     %Area of wall
-    A_wall = 
+    A_wall = 2*A_side + 2*A_top + ((2*(L_wall + L_ins) + depth) * (2*(L_wall + L_ins) + L_abs + height));
     %Area of insulator
     A_ins = (width * height * 2) + (sin(deg2rad(72)*height)*depth) + (width * depth);
     %Area of absorber
@@ -36,26 +44,35 @@ function dTdt = solarODE(t,T)
 
 
     %R absorber to air in
-    R0= 
+    R0= 1/(h_in*A_abs);
     %R air in to insulator
-    R1=
+    R1= 1/(h_in*A_ins);
     %R air in to window
-    R2= 
+    R2= 1/(h_in*A_win);
     %R through window
     R3 = 1/(h_win*A_win);
     %R through insulator
-    R4=
+    R4= L_ins/(k_ins*A_ins);
     %R through outer wall
-    R5=
+    R5= L_wall/(k_wall*A_wall);
     %R wall to air out
-    R6 = 
+    R6 = 1/(h_out*A_wall);
     %R window to air out
-    R7=
+    R7= 1/(h_out*A_win);
     %C absorber
-    C= 
+    C = (A_ins*L_ins)*2600*800;
+    
+    %Q of sun
+    q_sun = -361*(cos((pi*t)/(12*3600)))+224*(cos((pi*t)/(6*3600)))+210;
+    Qin = q_sun*(3.88*depth);
     
     
+    %Resistance Networks
+    R_win_total = (R2+R3+R7)^(-1);
+    R_wall_total = (R1+R4+R5+R6)^(-1);
+    R_total = R0+(R_win_total+R_wall_total)^(-1);
 
-    dTdt = []
+
+    dTdt = (Qin/C) - (((T_out-T)/R_total)/C);
 
 end
